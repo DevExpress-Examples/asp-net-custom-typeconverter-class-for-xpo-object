25161c79ac8a3fcece030572585b8e2624065300
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2023-10-28 09:45:53 +0400

    Update Default.aspx.cs

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -6,12 +6,9 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using DevExpress.Xpo;
 
-public partial class _Default : System.Web.UI.Page
-{
+public partial class _Default : System.Web.UI.Page {
     Session session;
-
-    protected void Page_Init(object sender, EventArgs e)
-    {
+    protected void Page_Init(object sender, EventArgs e) {
         session = new Session();
         xds.Session = session;
     }
